89b92b71d8927ef4021c9ccff43d4b86d06a0589
Nandha Sundaravadivel <nandhas@outlook.com>
2021-09-10 10:05:30 -0400

    Add bot mention, podium function

---
+++ helpers.js
@@ -0,0 +1,33 @@
+let podium = function(name1, name2, name3){
+	// find how big largest name is to size podium accordingly
+ 	var longestName = Math.max(name1.length, name2.length, name3.length);
+ 	
+	// pad whitespace onto shorter names so they are same length as longest
+  	for(let i = 0; i < arguments.length; i++){
+  		var currName = arguments[i];
+    	
+		if(currName.length < longestName + 2){ // plus 2 for spacing purposes
+    		var difference = longestName - currName.length;
+      		arguments[i] =  " ".repeat( Math.floor(difference/2) ) + arguments[i] + " ".repeat( Math.ceil(difference/2) );
+    	}
+	}
+  
+  	let pad = " ".repeat(longestName);
+  	var podiumTop = "@" + "-".repeat(longestName) + "@";
+  
+  	// build podium from top down.
+ 	var podium = "";
+  	podium += "  " + pad + name1 + "\n"; 				 
+  	podium += "  " + pad + podiumTop + "\n"; 	
+  	podium += " " + name2 + " |" + pad + "|\n"; 			
+  	podium += podiumTop + "|" + pad + "|\n";		
+  	podium += "|" + pad + "||" + pad + "| " + name3 + "\n";
+  	podium += "|" + pad + "||" + pad + "|" + podiumTop + "\n";
+  	podium += "|" + pad + "||" + pad + "||" + pad + "|\n";
+  	podium += "|" + pad + "||" + pad + "||" + pad + "|\n";
+  
+  	return podium;
+}
+//console.log(podium("Benjamin", "Max", "Walter"))
+
+exports.podium = podium;
--- index.js
+++ index.js
@@ -5,15 +5,26 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+const podium = require('./helpers.js');
+
 express.json({})
-
-
 
 /**Used to set up slack api verification**/
 
-// app.post('/', function(req, res){
-//     console.log(req.body);
-//     res.send("HTTP 200 OK Content-type: text/plain" + req.body.challenge);
-// });
+app.post('/', function(req, res){
+    console.log(req.body);
+    // Handles verifications for slack
+    if (req.body.challenge){
+        res.send("HTTP 200 OK Content-type: text/plain" + req.body.challenge);
+    }
 
-app.listen(3000)
+    // Handles bot mentions
+    else if (req.body.event.type == "app_mention") {
+        console.log("Bot Mentioned");
+        res.send(200)
+    }    
+});
+
+
+app.listen(3000, function(){console.log("Listening on port 3000")})
+
